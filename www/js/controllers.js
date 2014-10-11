4ad24fe31e820dba55b771e21adb42e2f69292c4
Francesco Sardara <sardara@netseven.it>
2014-10-11 15:09:41 +0200

    rendering maps

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,6 +1,21 @@
 angular.module('starter.controllers', [])
 
 .controller('DashCtrl', function($scope) {
+    angular.extend($scope, {
+      apuane: {
+        lat: 44.032,
+        lng: 10.31,
+        zoom: 4
+      },
+      tiles: {
+        url: 'http://www.webmapp.it/maps/alpi-apuane/mobile-map/tiles/{z}/{x}/{y}.png',
+        options: {
+          minZoom: 2,
+          maxZoom: 6,
+          attribution: '<a href="http://www.webmapp.it/mappa-delle-alpi-apuane-dati-cartografici-e-termini-duso/" target="_blank">Map data & terms</a>'
+        }
+      }
+    });
 })
 
 .controller('FriendsCtrl', function($scope, Friends) {
